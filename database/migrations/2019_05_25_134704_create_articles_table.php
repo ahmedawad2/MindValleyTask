e173e98e72bb6bcc0b84e6d22181adfc28a688ac
ahmed <ahmedawad1811@gmail.com>
2019-05-25 15:59:08 +0200

    added 2 migrations with models for articles and tags

---
+++ database/migrations/2019_05_25_134704_create_articles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('body');
+            $table->index('created_by');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('SET NULL');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropForeign('articles_created_by_foreign');
+            $table->dropIndex('articles_created_by_index');
+        });
+        Schema::dropIfExists('articles');
+    }
+}
